3121ffe1280b66613eb69a0853891f1ce6774c24
Pascal Andermatt <pascal.andermatt@students.fhnw.ch>
2021-05-22 23:44:26 +0200

    implemented first request

--- Frontend/src/Main.elm
+++ Frontend/src/Main.elm
@@ -7,26 +7,35 @@ import Html.Attributes exposing (..)
 import Json.Decode exposing (Decoder, at, list, string, succeed)
 import Http exposing (expectJson)
 
+import Json.Encode
+import Json.Decode exposing (field)
 
-{-
+type alias BankAccount =
+    { balance : Int
+    , active : Bool
+    , owner : String
+    , ibanNr : String
+    }
 
-Bitcoin Hype:
+decodeBankAccounts : Json.Decode.Decoder (List BankAccount)
+decodeBankAccounts = Json.Decode.list decodeBankAccount
 
-You want to get rich, and the best way to get rich fast is to invest all your money in bitcoins.
-To figure out if you are a millionaire yet, you want to build a small web application retrieving
-the current bitcoin value from a REST API and displaying it.
+decodeBankAccount : Json.Decode.Decoder BankAccount
+decodeBankAccount =
+    Json.Decode.map4 BankAccount
+        (field "balance" Json.Decode.int)
+        (field "active" Json.Decode.bool)
+        (field "owner" Json.Decode.string)
+        (field "ibanNr" Json.Decode.string)
 
-
-You are given the skeleton below. Fill it in to retrieve the current bitcoin price when the
-button is clicked.
-
-
-Optional:
-Load the bitcoin price immediately when the application is loaded.
-
-
--}
-
+encodeBankAccount : BankAccount -> Json.Encode.Value
+encodeBankAccount record =
+    Json.Encode.object
+        [ ("balance",  Json.Encode.int <| record.balance)
+        , ("active",  Json.Encode.bool <| record.active)
+        , ("owner",  Json.Encode.string <| record.owner)
+        , ("ibanNr",  Json.Encode.string <| record.ibanNr)
+        ]
 
 
 main : Program () Model Msg
@@ -40,82 +49,55 @@ main =
 
 
 type alias Model =
-  { currentPrice : Maybe String
+  { bankAccounts : List BankAccount
   }
-
 
 init : ( Model, Cmd Msg )
 init =
-  ( { currentPrice = Nothing }, getBitcoinPrice )
-
+  ( { bankAccounts = []}, Cmd.none )
 
 type Msg
   -- TODO add a second Msg constructor for the result returned by getBitcoinPrice
-  = GetBitcoinPrice | BitcoinPriceResult (Result Http.Error String)
-
+  = GetAllBankAccounts | BankAccountsResult (Result Http.Error (List BankAccount))
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
     -- TODO1: Execute the API request when the button is pressed
-    GetBitcoinPrice ->
-      ( model, getBitcoinPrice )
-    BitcoinPriceResult result  -> case result of
-       Ok price -> ({model | currentPrice = Just price}, Cmd.none)
-       Err httpError -> ({model | currentPrice = Nothing}, Cmd.none )
+    GetAllBankAccounts ->
+      ( model, getAllBankAccounts )
+    BankAccountsResult result  -> case result of
+       Ok listWithAccounts -> ({model | bankAccounts = listWithAccounts}, Cmd.none)
+       Err httpError -> ({model | bankAccounts = []}, Cmd.none )
     -- TODO2: Handle the result of the API request
 
-
-
--- TODO: Take a look at https://package.elm-lang.org/packages/elm/http/latest/Http#expectJson to see how
--- you can parse a JSON response. The actual JSON decoder is already provided for you, see decodeContent.
---
--- The API URL is http://api.coindesk.com/v1/bpi/currentprice.json
-getBitcoinPrice : Cmd Msg
-getBitcoinPrice = Http.get 
-                        { url = "http://api.coindesk.com/v1/bpi/currentprice.json", 
-                          expect = Http.expectJson BitcoinPriceResult decodeContent 
+getAllBankAccounts : Cmd Msg
+getAllBankAccounts = Http.get 
+                        { url = "http://localhost:4000/accounts", 
+                          expect = Http.expectJson BankAccountsResult decodeBankAccounts 
                         }
-
-decodeContent : Decoder String
-decodeContent =
-  at [ "bpi", "USD", "rate" ] string
-
-
----------------------------------------------------------------------------------------------------------------------------------
--- Feel free to take a look below, but no changes should be necessary
----------------------------------------------------------------------------------------------------------------------------------
-
 
 view : Model -> Html Msg
 view model =
   div [ class "container text-center jumbotron" ]
     -- Responsive fixed width container
-    [ viewMainContent model ]
+    [ 
+      button [ type_ "button", class "btn btn-primary", onClick GetAllBankAccounts ] [ text "get all accounts" ],
+      (viewMainContent model)
+    ]
 
 viewMainContent : Model -> Html Msg
 viewMainContent model =
-  div []
-    [ p [] [ h1 [] [ text "Bitcoin Price" ], h5 [] [ text "It costs right\n        now:" ] ]
-    , p [ class "alert alert-success" ]
-      [ h4 []
-        [ text
-          (Maybe.withDefault
-            "Not yet!"
-            model.currentPrice
-          )
-        , text " USD"
-        ]
-      ]
-    , button [ type_ "button", class "btn btn-primary", onClick GetBitcoinPrice ]
-      [ text "Update it now" ]
-    , div [ class "text-right small" ]
-      [ a [ href "https://github.com/dailydrip/elm-bitcoin-price" ]
-        [ text
-          "Source Code"
-        ]
-      ]
-    ]
+  div [] (List.map createBankAccountElement model.bankAccounts)
+    
 
+createBankAccountElement : BankAccount -> Html Msg
+createBankAccountElement acc = 
+          div []
+          [
+            p [] [text acc.ibanNr],
+            p [] [text acc.owner],
+            p [] [text (String.fromInt acc.balance)]
+          ]
 
--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -32,7 +32,12 @@ module.exports = {
   devServer: {
     inline: true,
     stats: 'errors-only',
-    historyApiFallback: true
+    historyApiFallback: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+   }
   },
 };
 
